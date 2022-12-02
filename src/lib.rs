845c9243f9c87a2340600b0bf8bbf10b50017e64
itowlson <ivan.towlson@fermyon.com>
2022-12-02 16:07:08 +1300

    New stuffs

---
+++ src/lib.rs
@@ -0,0 +1,15 @@
+use anyhow::Result;
+use spin_sdk::{
+    http::{Request, Response},
+    http_component,
+};
+
+/// A simple Spin HTTP component.
+#[http_component]
+fn devctest2(req: Request) -> Result<Response> {
+    println!("{:?}", req.headers());
+    Ok(http::Response::builder()
+        .status(200)
+        .header("foo", "bar")
+        .body(Some("Hello, Fermyon - I made a local edit".into()))?)
+}
