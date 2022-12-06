fb5faec04a2ecaa82efeae50b6c327377acb11a1
itowlson <ivan.towlson@fermyon.com>
2022-12-06 00:49:34 +0000

    Edited the branch in Codespaces

--- src/lib.rs
+++ src/lib.rs
@@ -11,5 +11,5 @@ fn devctest2(req: Request) -> Result<Response> {
     Ok(http::Response::builder()
         .status(200)
         .header("foo", "bar")
-        .body(Some("This is exciting new text on a branch".into()))?)
+        .body(Some("This is exciting new text on a branch, further edited in Codespaces".into()))?)
 }
